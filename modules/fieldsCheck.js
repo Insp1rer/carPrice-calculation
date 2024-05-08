9d0b5d192a2b78b8d2e0d96c219a8ec7ccb8332a
Insp1rer <igorkariymk@gmail.com>
2024-05-09 02:59:11 +0300

    carBrand slash remove

--- modules/fieldsCheck.js
+++ modules/fieldsCheck.js
@@ -1,51 +1,57 @@
 export function setupFieldsCheck() {
-  const carOwnerCheck = document.getElementById("owner");
-  const carAccidentsCheck = document.getElementById("accident");
-  const carMileageCheck = document.getElementById("mileage");
-  carOwnerCheck.onkeydown = (event) => {
+  const carOwnerCheck = document.getElementById('owner');
+  const carAccidentsCheck = document.getElementById('accident');
+  const carMileageCheck = document.getElementById('mileage');
+  const carBrandCheck = document.getElementById('brand');
+  carOwnerCheck.onkeydown = event => {
     if (
-      event.key === "-" ||
-      event.key === "." ||
-      event.key === "," ||
-      event.key === "e" ||
-      event.key === "+"
+      event.key === '-' ||
+      event.key === '.' ||
+      event.key === ',' ||
+      event.key === 'e' ||
+      event.key === '+'
     ) {
       return false;
     }
   };
-  carAccidentsCheck.onkeydown = (event) => {
+  carAccidentsCheck.onkeydown = event => {
     if (
-      event.key === "-" ||
-      event.key === "." ||
-      event.key === "," ||
-      event.key === "e" ||
-      event.key === "+"
+      event.key === '-' ||
+      event.key === '.' ||
+      event.key === ',' ||
+      event.key === 'e' ||
+      event.key === '+'
     ) {
       return false;
     }
   };
-  carMileageCheck.onkeydown = (event) => {
+  carMileageCheck.onkeydown = event => {
     if (
-      event.key === "-" ||
-      event.key === "." ||
-      event.key === "," ||
-      event.key === "e" ||
-      event.key === "+"
+      event.key === '-' ||
+      event.key === '.' ||
+      event.key === ',' ||
+      event.key === 'e' ||
+      event.key === '+'
     ) {
       return false;
     }
   };
-  carOwnerCheck.addEventListener("input", () => {
+  carBrandCheck.onkeydown = event => {
+    if (event.key === '/') {
+      return false;
+    }
+  };
+  carOwnerCheck.addEventListener('input', () => {
     if (carOwnerCheck.value.length > 3) {
       carOwnerCheck.value = carOwnerCheck.value.slice(0, 3);
     }
   });
-  carMileageCheck.addEventListener("input", () => {
+  carMileageCheck.addEventListener('input', () => {
     if (carMileageCheck.value.length > 4) {
       carMileageCheck.value = carMileageCheck.value.slice(0, 4);
     }
   });
-  carAccidentsCheck.addEventListener("input", () => {
+  carAccidentsCheck.addEventListener('input', () => {
     if (carAccidentsCheck.value.length > 3) {
       carAccidentsCheck.value = carAccidentsCheck.value.slice(0, 3);
     }
